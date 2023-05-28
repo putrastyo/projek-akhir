dbf6774e4370cac021dd93554b5bdaa77cd74512
Putra Setyo <107560080+putrastyo@users.noreply.github.com>
2023-05-28 16:27:14 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,221 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="assets/style/style.css">
+</head>
+
+<body class="bg-dark">
+
+    <!-- Login Admin -->
+    <div class="login position-absolute end-0 me-5 top-0 mt-5">
+        <a class="btn btn-info btn-sm" href=""> <span class="me-2">Admin</span><i class="bi bi-box-arrow-in-right"></a>
+    </div>
+    <!-- END Login Admin -->
+    <div class="container mt-5">
+        <div class="mb-5">
+            <h3 class="text-center text-white">Selamat Datang di <span class="text-info">Sistem Informasi Kelas</span></h3>
+            <p class=" text-light text-center m-auto" style="width: 700px;">Menjelajah dan mengelola data siswa dengan mudah dan efisien. Dengan Aplikasi Biodata Kelas, pengumpulan data siswa menjadi lebih mudah,cepat,dan terorganisir</p>
+        </div>
+
+        <div class="row mb-5">
+            <div class="col-6">
+                <form action="" method="post">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Cari...">
+                        <button class="btn btn-primary" type="button" name="cari"><i class="bi bi-search"></i></button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-6 d-flex justify-content-end">
+                <form class="me-5" action="" method="post">
+                    <label for="urutan" class="text-white">Urutkan :</label>
+                    <select name="urutan" id="urutan">
+                        <option disabled selected value="">Terbaru</option>
+                        <option value="">XII</option>
+                        <option value="">XI</option>
+                        <option value="">XII</option>
+                    </select>
+                </form>
+                <form action="" method="post">
+                    <label for="filter" class="text-white">Filter :</label>
+                    <select name="filter" id="filter">
+                        <option disabled selected value="">Jurusan</option>
+                        <option value=""></option>
+                        <option value=""></option>
+                        <option value=""></option>
+                    </select>
+                </form>
+            </div>
+        </div>
+
+        <!-- MAIN -->
+        <div class="row">
+            <!-- RPL -->
+            <p class="text-white fw-bold">Rekayasa Perangkat Lunak</p>
+            <hr class="border-white">
+
+            <div class="d-flex flex-wrap">
+                <a href="home/nama_murid.php" class="card text-center pt-3 pb-2 me-2 text-decoration-none text-dark" style="width: 10rem;">
+                    <div class="rounded-circle bg-primary m-auto" style="width: 60px; height: 60px"></div>
+                    <div class="card-body">
+                        <p class="fw-bold mb-1">XI RPL 1</p>
+                        <small class="text-muted">2022/2023</small>
+                    </div>
+                </a>
+                <a href="" class="card text-center pt-3 pb-2 me-2 text-decoration-none text-dark" style="width: 10rem;">
+                    <div class="rounded-circle bg-primary m-auto" style="width: 60px; height: 60px"></div>
+                    <div class="card-body">
+                        <p class="fw-bold mb-1">XI RPL 1</p>
+                        <small class="text-muted">2022/2023</small>
+                    </div>
+                </a>
+                <a href="" class="card text-center pt-3 pb-2 me-2 text-decoration-none text-dark" style="width: 10rem;">
+                    <div class="rounded-circle bg-primary m-auto" style="width: 60px; height: 60px"></div>
+                    <div class="card-body">
+                        <p class="fw-bold mb-1">XI RPL 1</p>
+                        <small class="text-muted">2022/2023</small>
+                    </div>
+                </a>
+                <a href="" class="card text-center pt-3 pb-2 me-2 text-decoration-none text-dark" style="width: 10rem;">
+                    <div class="rounded-circle bg-primary m-auto" style="width: 60px; height: 60px"></div>
+                    <div class="card-body">
+                        <p class="fw-bold mb-1">XI RPL 1</p>
+                        <small class="text-muted">2022/2023</small>
+                    </div>
+                </a>
+            </div>
+            
+            
+            <!-- AKL -->
+            <p class="text-white fw-bold mt-5">Akuntansi dan Keuangan Lembaga</p>
+            <hr class="border-white">
+
+            <div class="d-flex flex-wrap">
+                <a href="" class="card text-center pt-3 pb-2 me-2 text-decoration-none text-dark" style="width: 10rem;">
+                    <div class="rounded-circle bg-danger m-auto" style="width: 60px; height: 60px"></div>
+                    <div class="card-body">
+                        <p class="fw-bold mb-1">XI RPL 1</p>
+                        <small class="text-muted">2022/2023</small>
+                    </div>
+                </a>
+                <a href="" class="card text-center pt-3 pb-2 me-2 text-decoration-none text-dark" style="width: 10rem;">
+                    <div class="rounded-circle bg-danger m-auto" style="width: 60px; height: 60px"></div>
+                    <div class="card-body">
+                        <p class="fw-bold mb-1">XI RPL 1</p>
+                        <small class="text-muted">2022/2023</small>
+                    </div>
+                </a>
+                <a href="" class="card text-center pt-3 pb-2 me-2 text-decoration-none text-dark" style="width: 10rem;">
+                    <div class="rounded-circle bg-danger m-auto" style="width: 60px; height: 60px"></div>
+                    <div class="card-body">
+                        <p class="fw-bold mb-1">XI RPL 1</p>
+                        <small class="text-muted">2022/2023</small>
+                    </div>
+                </a>
+                <a href="" class="card text-center pt-3 pb-2 me-2 text-decoration-none text-dark" style="width: 10rem;">
+                    <div class="rounded-circle bg-danger m-auto" style="width: 60px; height: 60px"></div>
+                    <div class="card-body">
+                        <p class="fw-bold mb-1">XI RPL 1</p>
+                        <small class="text-muted">2022/2023</small>
+                    </div>
+                </a>
+            </div>
+
+            <!-- BDP -->
+            <p class="text-white fw-bold mt-5">Bisnis Daring dan Perdagangan</p>
+            <hr class="border-white">
+
+            <div class="d-flex flex-wrap">
+                <a href="" class="card text-center pt-3 pb-2 me-2 text-decoration-none text-dark" style="width: 10rem;">
+                    <div class="rounded-circle bg-success m-auto" style="width: 60px; height: 60px"></div>
+                    <div class="card-body">
+                        <p class="fw-bold mb-1">XI RPL 1</p>
+                        <small class="text-muted">2022/2023</small>
+                    </div>
+                </a>
+                <a href="" class="card text-center pt-3 pb-2 me-2 text-decoration-none text-dark" style="width: 10rem;">
+                    <div class="rounded-circle bg-success m-auto" style="width: 60px; height: 60px"></div>
+                    <div class="card-body">
+                        <p class="fw-bold mb-1">XI RPL 1</p>
+                        <small class="text-muted">2022/2023</small>
+                    </div>
+                </a>
+                <a href="" class="card text-center pt-3 pb-2 me-2 text-decoration-none text-dark" style="width: 10rem;">
+                    <div class="rounded-circle bg-success m-auto" style="width: 60px; height: 60px"></div>
+                    <div class="card-body">
+                        <p class="fw-bold mb-1">XI RPL 1</p>
+                        <small class="text-muted">2022/2023</small>
+                    </div>
+                </a>
+                <a href="" class="card text-center pt-3 pb-2 me-2 text-decoration-none text-dark" style="width: 10rem;">
+                    <div class="rounded-circle bg-success m-auto" style="width: 60px; height: 60px"></div>
+                    <div class="card-body">
+                        <p class="fw-bold mb-1">XI RPL 1</p>
+                        <small class="text-muted">2022/2023</small>
+                    </div>
+                </a>
+            </div>
+
+            <!-- OTKP -->
+            <p class="text-white fw-bold mt-5">Otomatisasi dan Tata Kelola Perkantoran</p>
+            <hr class="border-white">
+
+            <div class="d-flex flex-wrap">
+                <a href="" class="card text-center pt-3 pb-2 me-2 text-decoration-none text-dark" style="width: 10rem;">
+                    <div class="rounded-circle bg-warning m-auto" style="width: 60px; height: 60px"></div>
+                    <div class="card-body">
+                        <p class="fw-bold mb-1">XI RPL 1</p>
+                        <small class="text-muted">2022/2023</small>
+                    </div>
+                </a>
+                <a href="" class="card text-center pt-3 pb-2 me-2 text-decoration-none text-dark" style="width: 10rem;">
+                    <div class="rounded-circle bg-warning m-auto" style="width: 60px; height: 60px"></div>
+                    <div class="card-body">
+                        <p class="fw-bold mb-1">XI RPL 1</p>
+                        <small class="text-muted">2022/2023</small>
+                    </div>
+                </a>
+                <a href="" class="card text-center pt-3 pb-2 me-2 text-decoration-none text-dark" style="width: 10rem;">
+                    <div class="rounded-circle bg-warning m-auto" style="width: 60px; height: 60px"></div>
+                    <div class="card-body">
+                        <p class="fw-bold mb-1">XI RPL 1</p>
+                        <small class="text-muted">2022/2023</small>
+                    </div>
+                </a>
+                <a href="" class="card text-center pt-3 pb-2 me-2 text-decoration-none text-dark" style="width: 10rem;">
+                    <div class="rounded-circle bg-warning m-auto" style="width: 60px; height: 60px"></div>
+                    <div class="card-body">
+                        <p class="fw-bold mb-1">XI RPL 1</p>
+                        <small class="text-muted">2022/2023</small>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+    </div>
+    <div class="container-fluid mt-5" style="background: black; height: 300px;">
+        <div class="container">
+            <h4>Footer</h4>
+
+        </div>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
