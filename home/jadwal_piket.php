39a97abf69493e7c84c078d59ededf76e40d064a
raylathoriq <raylathoriq14@gmail.com>
2023-06-02 20:37:06 +0700

    Membuat halaman jadwal piket

---
+++ home/jadwal_piket.php
@@ -0,0 +1,349 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <link rel="stylesheet" href="../assets/style/style.css">
+
+</head>
+
+<body>
+<div class="navbar bg-danger">
+    <nav class="navbar navbar-expand-sm navbar-light w-100">
+      <div class="container">
+        <a class="navbar-brand" href="#">XI RPL 1 </a>
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+          <ul class="navbar-nav ms-auto mt-2 mt-lg-0 ull ">
+            <li class="nav-item lii">
+              <a class="nav-link " href="#" aria-current="page">Murid</a>
+            </li>
+            <li class="nav-item dropdown lii">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Jadwal
+              </a>
+              <ul class="dropdown-menu ull">
+                <li><a class="dropdown-item" href="#">Jadwal Pelajaran</a></li>
+                <li><a class="dropdown-item" href="#">Jadwal Piket</a></li>
+              </ul>
+            </li>
+            <li class="nav-item lii">
+              <a class="nav-link" href="#">Galeri</a>
+            </li>
+            <li class="nav-item lii">
+              <a class="nav-link" href="#">Tugas</a>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+    </nav>
+  </div>
+
+
+  <div class="container mt-4">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="#">Home</a></li>
+        <li class="breadcrumb-item">Jadwal Piket</li>
+      </ol>
+    </nav>
+
+    <div class="head-title mt-5">
+        <h4>Jadwal Piket</h4>
+    </div>
+
+
+    <div class="content">
+        <div class="row mt-5">
+            <div class="col-6">
+                <div class="table-responsive">
+                    <table class="table table-success table-bordered">
+                        <thead>
+                            <tr>
+                                <th colspan="3" scope="col" class="text-center bg-light">Senin</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="">
+                                <th scope="row">No Absen</th>
+                                <th>Nama</th>
+                                
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">1</td>
+                                <td>Parit</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">2</td>
+                                <td>Roy</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">3</td>
+                                <td>Ajank</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">4</td>
+                                <td>Setyo</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">5</td>
+                                <td>Juan</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">6</td>
+                                <td>Jahuwa</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">7</td>
+                                <td>Dani</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                
+            </div>
+            <div class="col-6">
+                <div class="table-responsive">
+                    <table class="table table-success table-bordered">
+                        <thead>
+                            <tr>
+                                <th colspan="3" scope="col" class="text-center bg-light">Selasa</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr class="">
+                                <th scope="row">No Absen</th>
+                                <th>Nama</th>
+                                
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">1</td>
+                                <td>Parit</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">2</td>
+                                <td>Roy</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">3</td>
+                                <td>Ajank</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">4</td>
+                                <td>Setyo</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">5</td>
+                                <td>Juan</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">6</td>
+                                <td>Jahuwa</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">7</td>
+                                <td>Dani</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mt-5">
+            <div class="col-6">
+                <div class="table-responsive">
+                    <table class="table table-success table-bordered">
+                        <thead>
+                            <tr>
+                                <th colspan="3" scope="col" class="text-center bg-light">Rabu</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="">
+                                <th scope="row">No Absen</th>
+                                <th>Nama</th>
+                                
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">1</td>
+                                <td>Parit</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">2</td>
+                                <td>Roy</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">3</td>
+                                <td>Ajank</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">4</td>
+                                <td>Setyo</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">5</td>
+                                <td>Juan</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">6</td>
+                                <td>Jahuwa</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">7</td>
+                                <td>Dani</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                
+            </div>
+            <div class="col-6">
+                <div class="table-responsive">
+                    <table class="table table-success table-bordered">
+                        <thead>
+                            <tr>
+                                <th colspan="3" scope="col" class="text-center bg-light">Kamis</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <tr class="">
+                                <th scope="row">No Absen</th>
+                                <th>Nama</th>
+                                
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">1</td>
+                                <td>Parit</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">2</td>
+                                <td>Roy</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">3</td>
+                                <td>Ajank</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">4</td>
+                                <td>Setyo</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">5</td>
+                                <td>Juan</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">6</td>
+                                <td>Jahuwa</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">7</td>
+                                <td>Dani</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-6 m-auto">
+                <div class="table-responsive">
+                    <table class="table table-success table-bordered">
+                        <thead>
+                            <tr>
+                                <th colspan="3" scope="col" class="text-center bg-light">Jumat</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="">
+                                <th scope="row">No Absen</th>
+                                <th>Nama</th>
+                                
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">1</td>
+                                <td>Parit</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">2</td>
+                                <td>Roy</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">3</td>
+                                <td>Ajank</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">4</td>
+                                <td>Setyo</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">5</td>
+                                <td>Juan</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">6</td>
+                                <td>Jahuwa</td>
+                            </tr>
+
+                            <tr class="">
+                                <td scope="row">7</td>
+                                <td>Dani</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+  </div>
+ 
+</body>
+
+</html>
